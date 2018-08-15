0b69dc780321bd8d1338bc563790ea17baf58e74
Amitava Mitra <mitraamitava7@gmail.com>
2018-08-15 12:53:12 +0000

    PE Bringup
    Signed-off-by: Amitava Mitra <mitraamitava7@gmail.com>

--- aosp_kenzo.mk
+++ aosp_kenzo.mk
@@ -17,27 +17,21 @@
 
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
-# Inherit some common cos stuff.
-$(call inherit-product, vendor/cos/common.mk)
-
-# Bootanimation
-TARGET_SCREEN_HEIGHT := 1080
-TARGET_SCREEN_WIDTH := 1920
+# Inherit some common Pixel-Experience stuffs.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_DENSITY=xhdpi
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := cos_kenzo
+PRODUCT_NAME := aosp_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-#Official
-KBUILD_BUILD_HOST := Saturn
-KBUILD_BUILD_USER := Savitar
 
 # Use the latest approved GMS identifiers unless running a signed build
 PRODUCT_BUILD_PROP_OVERRIDES += \
